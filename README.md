351f85248072d8a5b10befd749b7bef68e2554a2
Daniel Törbacka <daniel.torbacka@gmail.com>
2018-11-21 17:40:29 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Program to crawl svenska.se
+
+Purpose of this program is to gather the complete wordlist from svenska akademiens ordlista.
+
+
+### How to run the program
+I would recommend using pipenv
+
+```bash
+$ pipenv install
+```
+
+This will setup a pip environment and install all packages found in Pipfile. 
+
+```bash
+$ python client.py
+```
+
---
+++ client.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+
+data = {
+    'action': 'myprefix_scrollist',
+    'unik': '',
+    'dir': 'ned',
+    'dict': 'saol'
+}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
+}
+
+file = open("testfile.txt", "w")
+
+
+def main():
+    for i in range(1, 3000):
+        if 1000 % 100 == 0:
+            print(i)
+        response = requests.post('https://svenska.se/wp-admin/admin-ajax.php', data=data, headers=headers)
+        unik = parse_response(response)
+        if unik == -1:
+            break
+        data['unik'] = unik
+
+
+def parse_response(response):
+    soup = BeautifulSoup(response.text, features="html.parser")
+    links = soup.findAll("a", class_='slank')
+    if len(links) == 0:
+        return -1
+    for link in links:
+        gather_information(link)
+    div = soup.findAll("div", class_='pilned')
+    return div[0].a['unik']
+
+
+def gather_information(link):
+    span = link.findAll('span')
+    file.write(span[0].getText().strip() +
+               "," + span[1].getText().strip() +
+               "," + span[2].getText().strip() +
+               "," + span[3].getText().strip() +
+               "," + link['href'].strip() + "\n")
+
+
+if __name__ == '__main__':
+    main()
